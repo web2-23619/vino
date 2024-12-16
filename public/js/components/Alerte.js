475d05d86a19a54c3406d95b37adb6c60186320e
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-16 15:15:38 -0500

    retirer alerte apres 2.5 seconds

--- public/js/components/Alerte.js
+++ public/js/components/Alerte.js
@@ -3,17 +3,24 @@ class Alerte {
     #boutonAction;
 
     constructor(elementHtml) {
-		this.#elementHTML = elementHtml;
+        this.#elementHTML = elementHtml;
         this.#boutonAction = document.querySelector(
             "[data-js-action='fermer']"
         );
 
-		this.#boutonAction.addEventListener("click", this.#fermerAlerte.bind(this))
+        this.#boutonAction.addEventListener(
+            "click",
+            this.#fermerAlerte.bind(this)
+        );
+
+        setTimeout(() => {
+            this.#elementHTML.remove();
+        }, 2650);
     }
 
-	#fermerAlerte(){
-		this.#elementHTML.classList.add("invisible");
-	}
+    #fermerAlerte() {
+        this.#elementHTML.classList.add("invisible");
+    }
 }
 
 export default Alerte;
