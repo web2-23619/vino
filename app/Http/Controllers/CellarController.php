04eb3ccd15d11e8dfe9532a7dc164db307dcb26e
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-16 10:41:07 -0500

    Creer vue de liste de celiier pour 1 usager

---
+++ app/Http/Controllers/CellarController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cellar;
+use Illuminate\Http\Request;
+
+class CellarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+		$user_id = 3;
+		$cellars = Cellar::select()->where('user_id', $user_id)->orderBy('name')->get();
+        return view('cellar/index', ["cellars"=>$cellars]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cellar $cellar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cellar $cellar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cellar $cellar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cellar $cellar)
+    {
+		$cellar->delete();
+
+		return	redirect()->route('cellar.index')->with('success', 'Cellier supprimé avec succès');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CellarController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts/app');
 });
+
+
+Route::get('/cellar', [CellarController::class, 'index'])->name('cellar.index');
+Route::delete('/cellar/{task}', [CellarController::class, 'destroy'])->name('cellar.delete');
