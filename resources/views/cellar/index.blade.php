3281c70422346fa906653772a15be770b8c45651
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-16 12:48:03 -0500

    Appliquer style sur liste cellier

--- resources/views/cellar/index.blade.php
+++ resources/views/cellar/index.blade.php
@@ -8,13 +8,13 @@
 	@foreach($cellars as $cellar)
 	<article class="cellier">
 		<p>{{$cellar->name}}</p>
-		<button>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-			<title>Menu pour cellier</title>	
-			<path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" />
-			</svg>
-		</button>
-
+		<div class="menu-deroulant">
+			<input type="checkbox" aria-label="bouton pour ouvrir menu des actions">
+			<ul class="menu-deroulant__contenu">
+				<li><a class="button" href="">Modifier</a></li>
+				<li><button class="button">Supprimer</button></li>
+			</ul>
+		</div>
 	</article>
 	@endforeach
 </section>
