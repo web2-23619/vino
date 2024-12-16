ce14805e992567a04b77807ff7173e872e6039ea
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-16 11:00:27 -0500

    Appliquer style pour liste cellier

--- resources/views/cellar/index.blade.php
+++ resources/views/cellar/index.blade.php
@@ -6,11 +6,15 @@
 		<h2>Celliers</h2>
 	</header>
 	@foreach($cellars as $cellar)
-	<article class="cellar">
+	<article class="cellier">
 		<p>{{$cellar->name}}</p>
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+		<button>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+			<title>Menu pour cellier</title>	
 			<path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" />
-		</svg>
+			</svg>
+		</button>
+
 	</article>
 	@endforeach
 </section>
