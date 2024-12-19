345d8e315d40df44043252f5d7ade3d78f867fb8
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-12-19 16:10:00 -0500

    ajouter valeur par default pour quantite cellier

--- database/migrations/2024_12_12_190556_create_cellar_table.php
+++ database/migrations/2024_12_12_190556_create_cellar_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('cellars', function (Blueprint $table) {
             $table->id();
 			$table->string('name');
-			$table->integer('quantity');
+			$table->integer('quantity')->default(0);
 			$table->unsignedBigInteger('user_id');
             $table->timestamps();
 			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
