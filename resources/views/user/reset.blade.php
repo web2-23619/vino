b2a3552d901b120b77cd59f71ffcc02d2553d351
MaryamBoroughani <maryamboroughani@gmail.com>
2025-01-12 16:21:24 -0500

    modifier css

--- resources/views/user/reset.blade.php
+++ resources/views/user/reset.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- Formulaire Réinitialiser Mot de Passe -->
-<section>
+<section class="register-page">
     <header>
         <h2>Réinitialiser le mot de passe</h2>
     </header>
@@ -12,20 +12,20 @@
         @method('put')
 
         <!-- Champ Nouveau Mot de Passe -->
-        <div>
+        <div class="form-group">
             <label for="password">Nouveau mot de passe</label>
-            <input type="password" id="password" name="password" placeholder="Entrez votre nouveau mot de passe" required>
+            <input type="password" id="password" name="password" placeholder="Nouveau mot de passe" required>
         </div>
 
         <!-- Champ Confirmation Mot de Passe -->
-        <div>
+        <div class="form-group">
             <label for="password_confirmation">Confirmer le mot de passe</label>
             <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirmez votre mot de passe" required>
         </div>
 
         <!-- Bouton Soumettre -->
         <div>
-            <button type="submit">Réinitialiser le mot de passe</button>
+            <button type="submit" class="btn">Réinitialiser le mot de passe</button>
         </div>
     </form>
 </section>
