4e530ce58ae22adf40ad8bafad395aea661c1a67
MaryamBoroughani <maryamboroughani@gmail.com>
2025-01-14 07:20:17 -0500

    ajouter la route

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,6 @@ Route::middleware('auth:sanctum')->delete('supprimer/achat/{purchase}', [Purchas
 Route::delete('/supprimer/utilisateur/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 Route::middleware('auth:sanctum')->delete('supprimer/cellier/{cellar}', [CellarController::class, 'destroy']);
 Route::middleware('auth:sanctum')->delete('retirer/{cellar_id}/{bottle_id}', [CellarController::class, 'apiRemoveBottle']);
+
+Route::middleware('auth:sanctum')->post('/add-bottle', [CellarController::class, 'addBottle']);
+
