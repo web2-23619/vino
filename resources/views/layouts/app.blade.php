f11316b8196bcf9649a7ac15426ba634467cd5d4
JeffJ-L <e2396650@cmaisonneuve.qc.ca>
2025-01-15 10:11:30 -0500

    Remove CSRF token meta tag from layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>{{ config('app.name') }} - @yield('title')</title>
 	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 	<script type="module" src="@yield('js')"></script>
