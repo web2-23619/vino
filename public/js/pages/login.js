3966aa6c6de2a6e87920995a0a2691a7339fedc7
f-simard <e2396414@cmaisonneuve.qc.ca>
2025-01-14 21:10:46 -0500

    utiliser csfr dans le meta-tag

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -19,6 +19,7 @@ async function login(event) {
     event.preventDefault();
 
     try {
+		const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
         const email = document.querySelector("[name='email']").value;
         const password = document.querySelector("[name='password']").value;
 
@@ -26,6 +27,7 @@ async function login(event) {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "X-CSRF-TOKEN": csrfToken,
             },
             body: JSON.stringify({
                 email: email,
