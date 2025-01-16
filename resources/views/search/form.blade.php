5dc290232c00b46b0f891ad1a99f2c0347f7bb1f
MaryamBoroughani <maryamboroughani@gmail.com>
2025-01-16 18:09:16 -0500

    ajouter le bouton de loupe pour la page de recherche

--- resources/views/search/form.blade.php
+++ resources/views/search/form.blade.php
@@ -1,6 +1,6 @@
 <form class="form-group" action="{{ route('search.results') }}" method="POST">
 	@csrf
-	<div>
+	<div class="search-container">
 		<input
 			type="text"
 			id="search"
@@ -10,6 +10,10 @@
 			value="{{ request('query') }}"
 			required
 			autocomplete="off" />
-		<button type="submit" class="btn-search"></button>
+		<button type="submit" class="btn-search">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="16" height="16">
+				<path d="M10 2a8 8 0 015.61 13.61l4.2 4.2a1 1 0 01-1.42 1.42l-4.2-4.2A8 8 0 1110 2zm0 2a6 6 0 100 12 6 6 0 000-12z" />
+			</svg>
+		</button>
 	</div>
 </form>
