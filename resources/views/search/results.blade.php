44b78cac835a4e1c61d13c08726aadabb4512fab
MaryamBoroughani <maryamboroughani@gmail.com>
2025-01-16 15:22:30 -0500

    modifier le vue

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -39,11 +39,7 @@
                     <p>Prix: {{ number_format($bottle->price, 2) }} $</p>
                     <div class="card_bottle__actions">
                         <a href="{{ route('bottle.add', ['bottle_id' => $bottle->id]) }}" class="btn-add">Ajouter</a>
-                        <a href="{{ route('achat.add', ['bottle_id' => $bottle->id]) }}" class="btn-add">
-                            <svg enable-background="new 0 0 32 32" id="Glyph" version="1.1" viewBox="0 0 20 20" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="white">
-                                <title>Ajouter au panier</title>
-                                <path d="M4 4a1 1 0 0 1 1-1h1.5a1 1 0 0 1 .979.796L7.939 6H19a1 1 0 0 1 .979 1.204l-1.25 6a1 1 0 0 1-.979.796H9.605l.208 1H17a3 3 0 1 1-2.83 2h-2.34a3 3 0 1 1-4.009-1.76L5.686 5H5a1 1 0 0 1-1-1Z" clip-rule="evenodd" />
-                            </svg>
+                   
                         </a>
                     </div>
                 </div>
