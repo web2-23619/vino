1d8953189ecbc4e818fada2d566226303996203f
f-simard <e2396414@cmaisonneuve.qc.ca>
2025-01-22 21:27:40 -0500

    finaliser afficher achat par JS. boutons ok

--- public/js/components/Bottle.js
+++ public/js/components/Bottle.js
@@ -1,3 +1,6 @@
+import ModaleAction from "./ModaleAction.js";
+import App from "./App.js";
+
 export default class Bottle {
     #name;
     #price;
@@ -11,10 +14,12 @@ export default class Bottle {
     #page;
     #container;
     #elementHTML;
+    #btnRemove;
+    #btnReduire;
+    #btnAugmenter;
+    #changeQuantity;
 
     constructor(data, page, template, container) {
-        console.log(data);
-
         this.#name = data.bottle.name;
         this.#price = data.bottle.price;
         this.#country = data.bottle.country;
@@ -28,6 +33,7 @@ export default class Bottle {
         if (this.#page === "purchase") {
             this.#purchaseId = data.id;
             this.#quantity = data.quantity;
+            this.#changeQuantity = this.#changePurchaseQuantity;
         }
 
         this.#render();
@@ -35,8 +41,6 @@ export default class Bottle {
 
     #render() {
         const clone = this.#template.content.cloneNode(true);
-
-        console.log(clone);
 
         //remplacer valeur
 
@@ -57,5 +61,99 @@ export default class Bottle {
         this.#elementHTML.setAttribute("data-js-id", this.#purchaseId);
         this.#elementHTML.setAttribute("data-js-name", this.#name);
 
+        this.#btnRemove = this.#elementHTML.querySelector(
+            "[data-js-action='afficherModaleConfirmation']"
+        );
+
+        this.#btnRemove.addEventListener(
+            "click",
+            this.#afficherModaleSupressionAchat.bind(this)
+        );
+
+        this.#btnReduire = this.#elementHTML.querySelector(
+            "[data-js-action='reduire']"
+        );
+        this.#btnAugmenter = this.#elementHTML.querySelector(
+            "[data-js-action='augmenter']"
+        );
+
+        this.#btnReduire.addEventListener("click", (event) =>
+            this.#changeQuantity(event, "reduire")
+        );
+        this.#btnAugmenter.addEventListener("click", (event) =>
+            this.#changeQuantity(event, "augmenter")
+        );
+    }
+
+    #afficherModaleSupressionAchat(event) {
+        const declencheur = event.target;
+        const elToChange = declencheur.closest("article");
+        const purchaseID = elToChange.dataset.jsId;
+        const purchaseNom = elToChange.dataset.jsName;
+
+        const modale = new ModaleAction(
+            purchaseID,
+            purchaseNom,
+            "supprimerAchat",
+            "supprimer",
+            "achat",
+            elToChange
+        );
+    }
+
+    async #changePurchaseQuantity(event, action) {
+        console.log("clicked");
+
+        const csrfToken = document
+            .querySelector('meta[name="csrf-token"]')
+            .getAttribute("content");
+        const trigger = event.target;
+        const purchaseItem = trigger.closest("article");
+        const quantityElement = this.#elementHTML.querySelector(
+            "[data-js-quantite='quantite']"
+        );
+        let currentQuantity = parseInt(quantityElement.textContent);
+
+        // Ajuster la quantité selon l'action
+        if (action === "reduire" && currentQuantity > 1) {
+            currentQuantity--;
+        } else if (action === "augmenter") {
+            currentQuantity++;
+        }
+
+        // Envoie le PATCH request pour update seulement la quantité
+        const response = await fetch(
+            `${App.instance.baseURL}/api/achat/${this.#purchaseId}/quantite`,
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": csrfToken, // Ajoute CSRF token
+                    Authorization: "Bearer " + localStorage.getItem("token"), // Ajoute le token
+                },
+                body: JSON.stringify({
+                    quantity: currentQuantity,
+                }),
+            }
+        );
+
+        if (response.ok) {
+            // Mettre à jour le UI
+            quantityElement.textContent = currentQuantity;
+
+            // Desactiver le bouton "-" si la quantité est == 1
+            const btnReduire = purchaseItem.querySelector(
+                "[data-js-action='reduire']"
+            );
+            if (currentQuantity === 1) {
+                btnReduire.setAttribute("inert", "true");
+                btnReduire.classList.add("card_purchase_deactivated");
+            } else {
+                btnReduire.removeAttribute("inert");
+                btnReduire.classList.remove("card_purchase_deactivated");
+            }
+        } else {
+            // console.log("Échec.");
+        }
     }
 }
--- public/js/pages/purchase.js
+++ public/js/pages/purchase.js
@@ -4,39 +4,12 @@ import ModaleAction from "../components/ModaleAction.js";
 import Bottle from "../components/Bottle.js";
 
 (function () {
-    new App();
+    const appSingleton = new App();
 
     const alerte = document.querySelector(".alerte");
 
     if (alerte) {
         new Alerte(alerte);
-    }
-
-    const btnsReduire = document.querySelectorAll("[data-js-action='reduire']");
-    const btnsAugmenter = document.querySelectorAll(
-        "[data-js-action='augmenter']"
-    );
-
-    for (const btn of btnsReduire) {
-        btn.addEventListener("click", (event) =>
-            changeQuantity(event, "reduire")
-        );
-    }
-
-    for (const btn of btnsAugmenter) {
-        btn.addEventListener("click", (event) =>
-            changeQuantity(event, "augmenter")
-        );
-    }
-
-    const btnsModaleConfirmation = document.querySelectorAll(
-        "[data-js-action='afficherModaleConfirmation']"
-    );
-
-    if (btnsModaleConfirmation) {
-        for (const btn of btnsModaleConfirmation) {
-            btn.addEventListener("click", afficherModaleSupressionAchat);
-        }
     }
 
     document.addEventListener("fermerModale", function (event) {
@@ -85,8 +58,6 @@ async function showAll() {
 
     const data = await response.json();
 
-    console.log(data);
-
     const container = document.querySelector("[data-js-list]");
     const template = document.querySelector("template#bottle");
 
@@ -98,75 +69,4 @@ async function showAll() {
     } else {
         displayNoContentMessage();
     }
-}
-
-async function changeQuantity(event, action) {
-    const csrfToken = document
-        .querySelector('meta[name="csrf-token"]')
-        .getAttribute("content");
-    const trigger = event.target;
-    const purchaseItem = trigger.closest("article");
-    const id = purchaseItem.dataset.jsId;
-    const quantityElement = purchaseItem.querySelector(
-        "[data-js-quantite='quantite']"
-    );
-    let currentQuantity = parseInt(quantityElement.textContent);
-
-    // Ajuster la quantité selon l'action
-    if (action === "reduire" && currentQuantity > 1) {
-        currentQuantity--;
-    } else if (action === "augmenter") {
-        currentQuantity++;
-    }
-
-    // Envoie le PATCH request pour update seulement la quantité
-    const response = await fetch(
-        `${App.instance.baseURL}/api/achat/${id}/quantite`,
-        {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-TOKEN": csrfToken, // Ajoute CSRF token
-                Authorization: "Bearer " + localStorage.getItem("token"), // Ajoute le token
-            },
-            body: JSON.stringify({
-                quantity: currentQuantity,
-            }),
-        }
-    );
-
-    if (response.ok) {
-        // Mettre à jour le UI
-        quantityElement.textContent = currentQuantity;
-
-        // Desactiver le bouton "-" si la quantité est == 1
-        const btnReduire = purchaseItem.querySelector(
-            "[data-js-action='reduire']"
-        );
-        if (currentQuantity === 1) {
-            btnReduire.setAttribute("inert", "true");
-            btnReduire.classList.add("card_purchase_deactivated");
-        } else {
-            btnReduire.removeAttribute("inert");
-            btnReduire.classList.remove("card_purchase_deactivated");
-        }
-    } else {
-        // console.log("Échec.");
-    }
-}
-
-function afficherModaleSupressionAchat(event) {
-    const declencheur = event.target;
-    const elToChange = declencheur.closest("article");
-    const purchaseID = elToChange.dataset.jsId;
-    const purchaseNom = elToChange.dataset.jsName;
-
-    const modale = new ModaleAction(
-        purchaseID,
-        purchaseNom,
-        "supprimerAchat",
-        "supprimer",
-        "achat",
-        elToChange
-    );
 }
